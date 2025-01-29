5d20311a2e4a894a7edc13bcbb4b77235b11795a
faisan <faisan.m@cied.in>
2025-01-30 01:02:52 +0530

    Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -250,6 +250,35 @@ $(window).scroll(function () {
     items: 1
   });
 
+  // vegetable carousel
+  $(".vegetable-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    center: false,
+    dots: true,
+    loop: true,
+    margin: 25,
+    nav : true,
+    responsiveClass: true,
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+});
+
 })(jQuery);
 
 (function() {
